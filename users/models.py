6012f8a259587db962afcb1bc43612f54c3ef31b
paramesh <paramesh.itcareers@gmail.com>
2026-03-30 18:20:13 +0530

    Add skills and Render profiles completed

--- users/models.py
+++ users/models.py
@@ -22,3 +22,13 @@ class Profile(models.Model):
 
     def __str__(self):
         return str(self.user.username)
+    
+class Skills(models.Model):
+    owner = models.ForeignKey(Profile, on_delete=models.CASCADE, null=True, blank=True)
+    name = models.CharField(max_length=200, blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
+    created = models.DateTimeField(auto_now_add=True)
+    id= models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+
+    def __str__(self):
+        return str(self.name)
